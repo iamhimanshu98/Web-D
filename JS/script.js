8e63948bb98e59d7ab6e8fbaac44550f43db0c90
Himanshu Kumawat <himanshukumawat1313@gmail.com>
2025-03-01 20:45:20 +0530

    first commit

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+// let message = 'Hey dont do this!'
+// let warning = 'This could lead to a problem'
+// let error_message = 'See now! Error occured'
+
+
+// console.log(message)
+// // confirm('Do you really want this ?')
+// console.warn(warning)
+// // alert("Don't do this!")
+// console.error(error_message)
+
+// // prompt('Hey there')
+
+// const test = `${message}, \n${warning}, \n${error_message}`
+// console.log(test)
