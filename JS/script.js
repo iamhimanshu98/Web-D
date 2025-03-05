e3f17d3c0c0df064f5c7987314dc9c0db8c30716
Himanshu Kumawat <himanshukumawat1313@gmail.com>
2025-03-05 20:52:49 +0530

    loops in js

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,6 @@
 // let warning = 'This could lead to a problem'
 // let error_message = 'See now! Error occured'
 
-
 // console.log(message)
 // // confirm('Do you really want this ?')
 // console.warn(warning)
@@ -13,3 +12,39 @@
 
 // const test = `${message}, \n${warning}, \n${error_message}`
 // console.log(test)
+
+// var a = 9;
+
+// console.log(String(a));
+
+// greet();
+
+// function greet() {
+//   console.log("Hello!");
+// }
+
+// let helllo = function (b) {
+//   b++;
+//   console.log(b--);
+// };
+
+// helllo(5);
+
+// for (i = 0; i <= 7; i++) {
+//   console.log(i);
+// }
+
+a = 0;
+if (a == 0) {
+  console.log(a);
+} else if (a == 0) {
+  console.log(a++);
+} else {
+  console.log(a++);
+}
+
+object = [10, 20, 30, 40, 50];
+
+for (const element in object) {
+  console.log(object[element]);
+}
