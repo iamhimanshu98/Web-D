c85b2ba88d3e50eb549908f4d31132616006e89d
Himanshu Kumawat <himanshukumawat1313@gmail.com>
2025-03-27 14:23:23 +0530

    sum added

--- DSA/script.js
+++ DSA/script.js
@@ -1,35 +1,40 @@
 document.getElementById("factor").addEventListener("click", () => {
   let num = Number(prompt("enter a number"));
-
   let i = 0;
-
   while (i < num / 2) {
-    if (num % i == 0) console.log(`${i} * ${num / i} = ${num}`);
+    if (num % i === 0) console.log(`${i} * ${num / i} = ${num}`);
     i++;
   }
 });
 
-let primeChecker = () => {
-  let num = Number(prompt("enter a number"));
+let primeChecker = (num) => {
   let isPrime = true;
-  if(isNaN(num)){
-//   console.log("not a number.");
-  return num
-} else{
-    if(num <= 1) return false
-    if(num == 2) return isPrime
-    if (num % 2 == 0) return false;
-    for (let i = 3; i <= Math.floor(Math.sqrt(num)); i++) {
-        if (num % i == 0) return !isPrime;
-    }
-    
-    return isPrime;
-}
+  if (num <= 1) return false;
+  if (num === 2 || num === 3) return isPrime;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+  let count = 0;
+  for (let i = 5; i <= Math.floor(Math.sqrt(num)); i += 2) {
+    count++;
+    if (num % i === 0) return !isPrime;
+  }
+
+  return isPrime;
 };
 
 document.getElementById("prime").addEventListener("click", () => {
-    let ans = primeChecker()
-    if(isNaN(ans)) console.log("not a number.");
-    else if (ans) console.log("Prime");
-    else console.log("Not Prime");
+  let num = Number(prompt("enter a number"));
+  if (isNaN(num)) console.log("Invalid input");
+  else if (primeChecker(num)) console.log("Prime number");
+  else num >= 2 ? console.log("Composite number") : console.log("Not Prime");
+});
+
+function sum() {
+  let num1 = Number(prompt("enter num1"));
+  let num2 = Number(prompt("enter num2"));
+
+  console.log(num1 + num2);
+}
+
+document.getElementById("sum").addEventListener("click", () => {
+  sum();
 });
