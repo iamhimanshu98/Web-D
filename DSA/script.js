47b676db809cc968467e29ce544cf96ec13d0288
Himanshu Kumawat <himanshukumawat1313@gmail.com>
2025-03-27 10:05:09 +0530

    prime number

--- DSA/script.js
+++ DSA/script.js
@@ -8,3 +8,28 @@ document.getElementById("factor").addEventListener("click", () => {
     i++;
   }
 });
+
+let primeChecker = () => {
+  let num = Number(prompt("enter a number"));
+  let isPrime = true;
+  if(isNaN(num)){
+//   console.log("not a number.");
+  return num
+} else{
+    if(num <= 1) return false
+    if(num == 2) return isPrime
+    if (num % 2 == 0) return false;
+    for (let i = 3; i <= Math.floor(Math.sqrt(num)); i++) {
+        if (num % i == 0) return !isPrime;
+    }
+    
+    return isPrime;
+}
+};
+
+document.getElementById("prime").addEventListener("click", () => {
+    let ans = primeChecker()
+    if(isNaN(ans)) console.log("not a number.");
+    else if (ans) console.log("Prime");
+    else console.log("Not Prime");
+});
