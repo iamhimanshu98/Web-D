86f7f884a48d39d3b2ce7ac6bf75815208652ed8
Himanshu Kumawat <himanshukumawat1313@gmail.com>
2025-04-04 19:54:03 +0530

    pattern

--- DSA/script1.js
+++ DSA/script1.js
@@ -1,7 +1,9 @@
-let n = 5;
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= n; j++) {
-    process.stdout.write("* ");
-  }
-  console.log();
-}
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+
