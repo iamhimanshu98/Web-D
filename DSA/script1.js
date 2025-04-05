a08c5d9c44f9d99f3f683b6e32d07def73c429be
Himanshu Kumawat <himanshukumawat1313@gmail.com>
2025-04-05 23:32:25 +0530

    recursion pattern

--- DSA/script1.js
+++ DSA/script1.js
@@ -6,4 +6,21 @@
 //   console.log();
 // }
 
+function col(num) {
+  if (num === 0) {
+    console.log();
+    return;
+  }
+  process.stdout.write("* ");
+  col(num - 1);
+}
 
+function row(num) {
+  if (num === 0) {
+    return;
+  }
+  col(num);
+  row(num - 1);
+}
+
+row(5);
